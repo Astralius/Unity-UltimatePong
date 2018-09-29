3907307e284dcea025ff1e76ad186d44f8ae2d9d
Adrian Standowicz <adrian.standowicz@gmail.com>
2018-09-30 00:53:19 +0200

    FingersLite
    Added FingersLite asset for managing touch gestures on mobile.

---
+++ Assets/FingersLite/Script/ExtensionMethods.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class ExtensionMethods
+{ 
+    public static Vector2 ToWorld(this Vector2 screenPosition)
+    {
+        return Camera.main.ScreenToWorldPoint(screenPosition);
+    }
+}
