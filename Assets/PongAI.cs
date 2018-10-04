4719aad5f7ec7414ad5c9df6d352da817badab21
Adrian Standowicz <adrian.standowicz@gmail.com>
2018-10-04 21:57:25 +0200

    PongAI
    Added very simple AI that will cause computer-controlled paddles to always hit the ball.

---
+++ Assets/PongAI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PongAI : MonoBehaviour
+{
+    public Transform TheBall;
+
+    private new Transform transform;
+    private Vector3 newPosition;
+
+    private void Start()
+    {
+        this.transform = GetComponent<Transform>();      
+    }
+
+    private void Update()
+    {
+        newPosition = transform.parent.InverseTransformPoint(TheBall.position);
+        newPosition.x = Mathf.Clamp(newPosition.x, -3, 3);
+        newPosition.y = transform.localPosition.y;
+        newPosition.z = transform.localPosition.z;
+        transform.localPosition = newPosition;
+    }
+}
